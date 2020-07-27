20d9863f40ec55beb48b33066ecd60e910801e09
Guzo India <nishant.sharma6692@hotmail.com>
2020-07-27 11:08:05 +0530

    add categoies UI for shop page

--- lib/Shop.dart
+++ lib/Shop.dart
@@ -18,7 +18,94 @@ class _ShopState extends State<Shop> {
                   image: AssetImage('assets/images/walnuts.png'),
                   fit: BoxFit.cover),
             ),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+                  Colors.black.withOpacity(.8),
+                  Colors.black.withOpacity(.2),
+                ]),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(
+                            Icons.favorite,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.shopping_cart,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Our New Products",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30.0),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(children: <Widget>[
+                            Text("VIEW MORE",
+                                style: TextStyle(color: Colors.white)),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.white,
+                              size: 15,
+                            ),
+                          ]),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(20.0,),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Categories", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+                    Text("All")
+                  ],
+                ),
+                SizedBox(height: 10.0,),
+                Column(
+                  
+                )
+              ],
+            ),
+            
+
+
+          )
         ],
       ),
     );
