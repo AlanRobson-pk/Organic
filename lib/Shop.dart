10d4c7478c33ada9095c073289d9e31cc957823e
Guzo India <nishant.sharma6692@hotmail.com>
2020-07-27 17:51:43 +0530

    updated prdouct showcase

--- lib/Shop.dart
+++ lib/Shop.dart
@@ -1,7 +1,7 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:prajnaa_organic_store/animation.dart';
+
+import 'main.dart';
 
 class Shop extends StatefulWidget {
   @override
@@ -15,157 +15,198 @@ class _ShopState extends State<Shop> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            FadeAnimation(1, Container(
-              height: 500,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/prajnaa-dryfruits.png'),
-                    fit: BoxFit.cover),
-              ),
-              child: Container(
+            FadeAnimation(
+              1,
+              Container(
+                height: 400,
                 decoration: BoxDecoration(
-                  gradient:
-                      LinearGradient(begin: Alignment.bottomRight, colors: [
-                    Colors.black.withOpacity(.8),
-                    Colors.black.withOpacity(.2),
-                  ]),
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/product-page.jpg'),
+                      fit: BoxFit.cover),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 50.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          FadeAnimation(1.2, IconButton(
-                            icon: Icon(
-                              Icons.favorite,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {},
-                          ),),
-                          FadeAnimation(1.3, IconButton(
-                            icon: Icon(
-                              Icons.shopping_cart,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {},
-                          ),
-                          ),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient:
+                        LinearGradient(begin: Alignment.bottomRight, colors: [
+                      Colors.black.withOpacity(.8),
+                      Colors.black.withOpacity(.2),
+                    ]),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 50.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
                           children: <Widget>[
-                            FadeAnimation(1.4, Text(
-                              "Our New Products",
-                              style: TextStyle(
+                            FadeAnimation(
+                              1.2,
+                              IconButton(
+                                icon: Icon(
+                                  Icons.arrow_back,
                                   color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 30.0),
-                            ),),
-                            SizedBox(
-                              height: 10,
+                                ),
+                                onPressed: () {
+                                  // Navigator.push(
+                                  //   context,
+                                  //   MaterialPageRoute(
+                                  //       builder: (context) => HomePage()),
+                                  // );
+                                  Navigator.pop(context);
+                                },
+                              ),
                             ),
-                            FadeAnimation(1.6, Row(children: <Widget>[
-                              Text("VIEW MORE",
-                                  style: TextStyle(color: Colors.white)),
-                              SizedBox(
-                                height: 5,
+                            SizedBox(
+                              width: 240.0,
+                            ),
+                            FadeAnimation(
+                              1.2,
+                              IconButton(
+                                icon: Icon(
+                                  Icons.favorite,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {},
                               ),
-                              Icon(
-                                Icons.arrow_forward_ios,
-                                color: Colors.white,
-                                size: 15,
+                            ),
+                            FadeAnimation(
+                              1.3,
+                              IconButton(
+                                icon: Icon(
+                                  Icons.shopping_cart,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {},
                               ),
-                            ]),),
+                            ),
                           ],
                         ),
-                      )
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              FadeAnimation(
+                                1.4,
+                                Text(
+                                  "Prajnaa's Products",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 30.0),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              FadeAnimation(
+                                1.6,
+                                Row(children: <Widget>[
+                                  Text("VIEW MORE",
+                                      style: TextStyle(color: Colors.white)),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.white,
+                                    size: 15,
+                                  ),
+                                ]),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),),
+            ),
             Padding(
               padding: const EdgeInsets.all(
                 20.0,
               ),
-              child: FadeAnimation(1.7, Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Categories",
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                      Text("All")
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    height: 150,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
+              child: FadeAnimation(
+                1.7,
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        makeCategory(
-                            image: 'assets/images/prajnaa-spices.jpg', title: 'Spices'),
-                        makeCategory(
-                            image: 'assets/images/prajnaa-herbs.jpg',
-                            title: 'Herbs'),
-                        makeCategory(
-                            image: 'assets/images/prajnaa-dryfruits.png',
-                            title: 'Dry Fruits'),
-                        makeCategory(
-                            image: 'assets/images/prajnaa-fruits.jpg', title: 'Fruits'),
+                        Text(
+                          "Categories",
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
+                        ),
+                        Text("All")
                       ],
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Prajnaa Best Selling",
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      height: 150,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          makeCategory(
+                              image: 'assets/images/prajnaa-spices.jpg',
+                              title: 'Spices'),
+                          makeCategory(
+                              image: 'assets/images/prajnaa-herbs.jpg',
+                              title: 'Herbs'),
+                          makeCategory(
+                              image: 'assets/images/prajnaa-dryfruits.png',
+                              title: 'Dry Fruits'),
+                          makeCategory(
+                              image: 'assets/images/prajnaa-fruits.jpg',
+                              title: 'Fruits'),
+                        ],
                       ),
-                      Text("All")
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    height: 150,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        makeBestCategory(
-                            image: 'assets/images/prajnaa-spices.jpg', title: 'Spices'),
-                        makeBestCategory(
-                            image: 'assets/images/prajnaa-herbs.jpg',
-                            title: 'Herbs'),
-                        makeBestCategory(
-                            image: 'assets/images/prajnaa-dryfruits.png',
-                            title: 'Dry Fruits'),
-                        makeBestCategory(
-                            image: 'assets/images/prajnaa-fruits.jpg', title: 'Fruits'),
+                        Text(
+                          "Prajnaa Best Selling",
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
+                        ),
+                        Text("All")
                       ],
                     ),
-                  )
-                ],
-              ),),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      height: 150,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          makeBestCategory(
+                              image: 'assets/images/turmeric.png',
+                              title: 'Turmeric'),
+                          makeBestCategory(
+                              image: 'assets/images/almonds.jpeg',
+                              title: 'Almonds'),
+                          makeBestCategory(
+                              image: 'assets/images/mulethi.png',
+                              title: 'Liquorice'),
+                          makeBestCategory(
+                              image: 'assets/images/walnuts.png',
+                              title: 'Walnuts'),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ),
@@ -180,8 +221,7 @@ class _ShopState extends State<Shop> {
         margin: EdgeInsets.only(right: 20),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-              image: AssetImage(image), fit: BoxFit.cover),
+          image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
         ),
         child: Container(
           padding: EdgeInsets.all(10.0),
@@ -209,15 +249,14 @@ class _ShopState extends State<Shop> {
     );
   }
 
-  Widget makeBestCategory({image,title}) {
+  Widget makeBestCategory({image, title}) {
     return AspectRatio(
       aspectRatio: 3 / 2.2,
       child: Container(
         margin: EdgeInsets.only(right: 20),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-              image: AssetImage(image), fit: BoxFit.cover),
+          image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
         ),
         child: Container(
           padding: EdgeInsets.all(10.0),
